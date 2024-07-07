4ae1bacbc08252691ab4dd5198cee1b588293ed8
Danil Klimenko <sos.danil@yandex.ru>
2024-07-07 21:16:11 +0500

    сделал контроллеры на пользователелей

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('create/', UserCreateAPIView.as_view(), name='users_create'),
+    path('', UserListAPIView.as_view(), name='users_list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='users_retrieve'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='users_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        obj = serializer.save(is_active=True)
+        obj.set_password(self.request.data['password'])
+        obj.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
